1a3a1f0b4372d974983464143833fd211ef53edf
Lucas <lucs.25@hotmail.com>
2022-09-26 18:42:06 -0300

    Send archives modify

--- Passos.js
+++ Passos.js
@@ -1,10 +1,12 @@
+let ini = document.getElementById("txi")
+let fim = document.getElementById("txf")
+let passo = document.getElementById("txp")
+let res = document.getElementById("res")
+let logoutBtn =document.getElementById("logout")
+
+logout.style.display = "none"
 
 function contar() {
-    let ini = document.getElementById("txi")
-    let fim = document.getElementById("txf")
-    let passo = document.getElementById("txp")
-    let res = document.getElementById("res")
-
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('Favor preencher todos os campos!')
     } else {
@@ -17,14 +19,46 @@ function contar() {
             p = 1
         }
         if ( i < f) {//contagem crescente
+            logout.style.display = "block"
+            document.getElementById("contar").style.display = "none"
             for (let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F47D}`
             }	
         } else {//contagem regressiva
+            logout.style.display = "block"
+            document.getElementById("contar").style.display = "none"
             for (let c = i; c >= f; c -= p) {
-            res.innerHTML += `${c} \u{1F47D}` 
+                res.innerHTML += `${c} \u{1F47D}` 
             }
             res.innerHTML += `\u{1F6F8}`
         }
     }
 }
+
+logoutBtn.addEventListener("click", () => {
+    document.getElementById("txi").value =" " ;
+    document.getElementById("txf").value =" ";
+    document.getElementById("txp").value =" ";
+    document.getElementById("res").innerText = "Preparando para contar...";
+
+    logout.style.display = "none"
+    document.getElementById("contar").style.display = "block"
+    
+});
+
+/* 
+Outro método para resetar valores - lembrando de adicionar evento onclick="myFunction()" no button logou no html
+
+function myFunction () {
+    document.getElementById("txi").value ="" ;
+    document.getElementById("txf").value ="";
+    document.getElementById("txp").value ="";
+    document.getElementById("res").innerText = "";
+
+    logout.style.display = "none"
+    document.getElementById("contar").style.display = "block"
+};
+
+contar() */
+
+
